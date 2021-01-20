d441a693f56c6e117f2c6a247baf62e212b0f78d
Mark Gerald Calacal <mcalacal@stratpoint.com>
2021-01-20 13:05:41 +0800

    Changed mysql driver package in order to support mysql 8

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const database = require('./config/database');
