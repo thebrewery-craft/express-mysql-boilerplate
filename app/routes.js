be3d4d6b6a357330330b7a380d517a49d7842fff
Mark Gerald Calacal <mcalacal@stratpoint.com>
2021-01-21 16:08:14 +0800

    Added health check route

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,19 @@
 module.exports = function(app) {
+  /**
+   * API routes
+   */
   app.get('/api', function(req, res) {
       res.json({
         message: 'Welcome to The Brewery'
       })
   });
+  
+  /**
+   * Health Check route 
+   * */  
+  app.get('/health', function(req, res) {
+    res.json({
+      status: 'Healthy'
+    })
+});
 };
